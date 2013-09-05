4943186850a2ffba480a7c39bb54fecbad4dd4c5
Brian Abelson <brianabelson@gmail.com>
2013-09-04 21:25:24 -0300

    highlighting functionality is now working

---
+++ app/js/app.js
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>D3</title>
+    <meta charset="utf-8">
+    <style>
+    </style>
+    </head>
+    <body>
+        <div>Hello</div>
+        <div>Hi</div>
+        <script src="http://d3js.org/d3.v3.min.js"></script>
+        <script>
+            d3.selectAll('div')
+                .style("background-color", "red");
+        </script>
+    </body>
+</html>
