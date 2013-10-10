13cf135801f6c57369766b1f246deb4d67101309
Sonya Song <sonya2song+github@gmail.com>
2013-10-10 14:00:28 -0400

    further removed \xff chars and improved matching; added 3 charts

--- round4/analysis.R
+++ round4/analysis.R
@@ -2,17 +2,19 @@ df <- read.csv('responses_v10.csv', encoding="UTF-8", header=T, as.is=T)
 library('rjson')
 library('stringr')
 library('ggplot2')
+library('reshape2')
+library('colorspace')
 names(df) <- c(
     "coder",
-    "v1",
-    "v2",
-    "v3",
-    "v4",
-    "v5",
-    "v6",
-    "v7",
-    "v8",
-    "v9",
+    "v01",
+    "v02",
+    "v03",
+    "v04",
+    "v05",
+    "v06",
+    "v07",
+    "v08",
+    "v09",
     "v10",
     "url",
     "content",
@@ -25,29 +27,16 @@ names(df) <- c(
 # str(df)
 
 sj <- c(0, 1, 0, 1, 1, 0, 0, 1, 0, 1)
-nsj <- 1 - sj
+qj <- 1 - sj
 
 df[df=="Yes" | df=="Clearly"] <- 2
 df[df=="Somewhat"] <- 1
 df[df=="No"] <- 0
-head(df[,2:11])
 
-# for (i in c(1:12,17)){
-#   df[,i]<-factor(df[,i])  
-# }
-# summary(df[,11])
-# for (i in 2:9){
-# #   df[,i]<-factor(df[,i], levels(df[,i])[c(2,3,1)])  
-#   df[,i]<-ordered(df[,i], levels=c("No", "Somewhat", "Clearly"))  
-# }
-# summary(df[,9])
-# for (i in 10:11){
-#   levels(df[,i])<-c("Somewhat","Yes","No")
-# #   df[,i]<-factor(df[,i], levels(df[,i])[c(3,1,2)])  
-#   df[,i]<-ordered(df[,i], levels=c("No", "Somewhat", "Yes"))  
-# }
-# summary(df[,11])
-# sum(as.numeric(df[1,2:11]))
+for (i in 2:11){
+  df[,i]<-as.numeric(df[,i])  
+}
+summary(df[,2:11])
 
 tag_df <- data.frame()
 # str(tag_df)
@@ -75,13 +64,20 @@ for (i in 1:nrow(df)) {
 #   summary(tag_result)
   
   for (j in 1:nrow(tags)) {
-#     j <- 3
-    start_pattern <- substring(tag_text[j], 1, 20)
-#     Encoding(start_pattern) <- "UTF-8"
-#     print(start_pattern)
-    start_pattern <- gsub("^[^0-9a-zA-Z]*", "", start_pattern, perl=F)
-#     print(start_pattern)
-    start <- str_locate(text, start_pattern)[1]
+#     j <- 4
+    print(j)
+    start_pattern <- substring(tag_text[j], 1, 30)
+    print(start_pattern)
+    start_pattern <- iconv(start_pattern, "UTF-8", "latin1", "?")
+    start_pattern <- gsub("^[^0-9a-zA-Z]*", "", start_pattern, perl=F)    
+    print(start_pattern)
+    start_pattern <- strsplit(start_pattern, "\\?")[[1]][1]
+#     start_pattern <- gsub("\\?*$", "", start_pattern, perl=F)
+#     start_pattern <- substring(start_pattern, 1, str_locate(start_pattern, fixed("?"))[1]-1)
+    print(start_pattern)
+#   escape special chars in the pattern, such as ? etc. 
+#     str_locate(text, fixed(start_pattern))
+    start <- str_locate(text, fixed(start_pattern))[1]
     tag_length <- nchar(tag_text[j])
     end <- start + tag_length
     temp <- tags$label[j]
@@ -89,21 +85,19 @@ for (i in 1:nrow(df)) {
     print (c(j, temp, start, start_pattern))
   }
   
-  tag_result
+  tag_result <- rbind(tag_result, data.frame(label=c("solution","result","problem"), start=rep(text_length,3), length=rep(0,3), end=rep(text_length,3)))
+  
   tag_result$start <- as.numeric(tag_result$start)
   tag_result$length <- as.numeric(tag_result$length)
   tag_result$end <- as.numeric(tag_result$end)
+  print(tag_result)
   
-#   tag_result$label
   per_tag <- data.frame(tapply(tag_result$length, tag_result$label, sum) / text_length * 100)
-  str(per_tag)
-  per_tag
-  
-  min_tag <- data.frame(tapply(tag_result$start, tag_result$label, min) / text_length * 100)
-  str(min_tag)
-  min_tag
-  
-  first_mention <- rownames(min_tag)[which(min_tag==min(min_tag[!is.na(min_tag),]))]
+  min_tag <- data.frame(tapply(tag_result$start, tag_result$label, min) / text_length * 100) 
+  print(per_tag)
+  print(min_tag)
+#   first_mention <- rownames(min_tag)[which(min_tag==min(min_tag[!is.na(min_tag),]))]
+  first_mention <- rownames(min_tag)[which(min_tag==min(min_tag))]
   print (first_mention)
 
   tag_stats <- data.frame(
@@ -117,13 +111,78 @@ for (i in 1:nrow(df)) {
   )
   
 #   str(tag_stats)
+#   str(tag_df)
   tag_df <- rbind(tag_df, tag_stats)
-#   tag_df
 }
 
 df <- cbind(df, tag_df)
-sj_score <- data.matrix(df[,2:11]) %*% cbind(sj,nsj)
+sj_score <- data.matrix(df[,2:11]) %*% cbind(sj,qj)
+# summary(sj_score)
 df <- cbind(df, sj_score)
 
-# str(df)
-write.csv(df, "responses_with_tags.csv", row.names=F, sep="\t")
+write.csv(df, "responses_with_tags.csv", row.names=F)
+
+
+#### solutions journalism vs. single items ####
+
+names(df)
+# df_num <- df[,c(2:11, 18:23, 25, 26)]
+df_num <- df[,c(2:11, 25, 26)]
+str(df_num)
+
+df_num_m <-melt(cor(df_num))
+names(df_num_m) <- c("Var1", "Var2", "Correlation")
+qplot(x=Var1, y=Var2, data=df_num_m, fill=Correlation, geom="tile")+
+  xlab("")+
+  ylab("")+
+  ggtitle("Correlation Matrix of Rubric Parameters")
+  
+
+#### solutions journalism vs. quality of journalism (Inter-coder reliability) ####
+
+sj_score <- data.frame(coder=df$coder, headline=df$headline, score=df$sj, dimension=rep("Solutions Journalism",nrow(df)))
+qj_score <- data.frame(coder=df$coder, headline=df$headline, score=df$qj, dimension=rep("Quality of Journalism",nrow(df)))
+score <- rbind(sj_score, qj_score)
+# score
+
+jitter <- .3
+
+ggplot(score, aes(y=headline))+
+  geom_point(aes(x=score, color=coder, shape=dimension, size=dimension), position=position_jitter(width=jitter, height=0))+
+  scale_shape_manual(values=c(18,21))+
+  scale_size_manual(values=c(15,10))+
+  ggtitle("Inter-coder Reliability (Solutions Journalism vs. Quality of Journalism)")+
+  theme(
+    plot.title = element_text(size=22, face="bold"), 
+    legend.position="top", 
+    legend.title = element_text(size=18, face="bold"), 
+    legend.text = element_text(size = 18),
+    axis.title.x=element_text(face="bold", size=18), 
+    axis.title.y=element_text(face="bold", size=18), 
+    axis.text.y=element_text(size=15, vjust=.5)
+    )
+
+
+#### percentage of sj vs. position of first solution tag ####
+
+sj_per <- data.frame(coder=df$coder, per_solution=df$per_solution, min_solution=df$min_solution, score=df$sj, dimension=rep("Solutions Journalism",nrow(df)))
+qj_per <- data.frame(coder=df$coder, per_solution=df$per_solution, min_solution=df$min_solution, score=df$qj, dimension=rep("Quality of Journalism",nrow(df)))
+per <- rbind(sj_per, qj_per)
+summary(per[per$coder == 'David Bornstein',])
+per$per_solution_int <- factor(floor(per$per_solution/16))
+per$per_solution_int
+
+ggplot(per, aes(x=min_solution))+
+  geom_point(aes(y=score, color=coder, shape=dimension, size=per_solution_int), position=position_jitter(width=jitter, height=0))+
+  scale_shape_manual(values=c(18,21))+
+  scale_size_manual(values=1:7*4)+
+  ggtitle("Position of First Solution Tag vs. Score & Percentage of Solution Tags in Text")+
+  theme(
+    plot.title = element_text(size=22, face="bold"), 
+    legend.position="top", 
+    legend.title = element_text(size=18, face="bold"), 
+    legend.text = element_text(size = 18),
+    axis.title.x=element_text(face="bold", size=18), 
+    axis.title.y=element_text(face="bold", size=18), 
+    axis.text.y=element_text(size=15, vjust=.5)
+  )
